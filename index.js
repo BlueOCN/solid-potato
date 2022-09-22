dc21bf6f11b1dae17d27ff7c087e63fe7a9142b4
BlueOCN <107767647+BlueOCN@users.noreply.github.com>
2022-09-21 20:54:41 -0500

    examen

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //  Importar bibliotecas
 const express = require('express');
 const path = require('path');
-const consolaRoutes = require('./routes/consola')
+const consolaRoutes = require('./routes/examen')
 const app = express();
 
 // const consolaRoutes = require('./routes/consola');
@@ -10,48 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/consola',consolaRoutes);
-
-// Recursos
-// Definición de nuestra aplicación en localhost/bigote
-app.get('/bigote',(request,response)=>{
-    console.log('Hola mundo!');
-    response.send('<h1>Hola Mundo!</h1>');
-});
-
-// Definición de nuestra aplicación en localhost
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'views','index.html'));
-});
-
-app.get('/home', (req,res)=>{
-    res.sendFile(path.join(__dirname,'views','index1.html'));
-});
-
-app.get('/profile', (req,res)=>{
-    res.sendFile(path.join(__dirname,'views','index2.html'));
-});
-
-// Prueba query
-app.get('/prueba1', (req,res)=>{
-    console.log(req.query);
-    res.send("Datos enviados por querry "+req.query.name);
-});
-
-// Prueba params
-app.get('/prueba2/:name/:age', (req,res)=>{
-    console.log(req.params);
-    console.log(req.query);
-    res.send("Datos enviados por params: " + req.params.name);
-    res.send("Datos enviados por querry " + req.query.país);
-});
-
-app.post('/prueba3',(req,res)=>{
-    console.log(req.body)
-    res.send("Datos recibidos!")
-});
+app.use('/examen',consolaRoutes);
 
 // Lanzar la aplicación
-app.listen(8080,()=>{
+app.listen(8081,()=>{
     console.log('Servidor en línea!');
 });
